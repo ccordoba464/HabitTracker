8b146346a0a95e218c9d7b5f3609778488180706
ccordoba464 <93160733+ccordoba464@users.noreply.github.com>
2023-12-23 16:32:34 -0500

    Add new packages and header/home components

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import styles from "./Home.module.css";
+
+export default function Home() {
+  return (
+    <body className={styles.home}>
+      <div className={styles.title}>Tornado</div>
+    </body>
+  );
+}
